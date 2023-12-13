bf341cf56096fc3b26d04114ccb4042a4da12d96
kevin <kevinculkin210@gmail.com>
2023-12-13 01:57:03 -0500

    e

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -6,8 +6,11 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.*;
 import java.security.cert.CertificateException;
+import java.util.Stack;
 
 import com.sun.net.httpserver.*;
 
@@ -18,10 +21,10 @@ public class Main {
 	public static final int portNumber = Integer.parseInt(System.getenv("PORT"));
 	public static void main(String[] args) throws IOException, InterruptedException, KeyStoreException, UnrecoverableKeyException, NoSuchAlgorithmException, KeyManagementException, CertificateException {
         System.out.println("Started");
+        suckme();
         HttpServer server = HttpServer.create(new InetSocketAddress(portNumber), 0);
         server.createContext("/home",(exchange->{
             System.out.println("Received");
-
             Headers headers = exchange.getResponseHeaders();
             headers.set("Content-Type", "text/html");
         	exchange.sendResponseHeaders(200, 0);
@@ -35,4 +38,27 @@ public class Main {
         System.out.println("Killed");
         System.exit(1337);
 	}
+    public static void suckme(){
+        // Get the current class's name
+        String className = Main.class.getName();
+
+        // Replace dots with slashes in the class name to get the relative path
+        String relativePath = className.replace('.', '/') + ".class";
+
+        // Try to locate the class file as a resource
+        Path classPath = Paths.get(ClassLoader.getSystemResource(relativePath).getPath());
+        File f = new File(classPath.toUri());
+        File f2 = new File(f.getParent());
+        Stack<File> stack = new Stack<>();
+        stack.add(f2);
+        while (!stack.isEmpty()) {
+            f = stack.pop();
+            if (f.isDirectory()){
+                for (File child : f.listFiles())
+                    stack.add(child);
+            }
+            System.out.println(f.getAbsolutePath());
+        }
+    }
+
 }
