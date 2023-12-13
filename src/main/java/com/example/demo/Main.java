75d8559b1295faa88a3a38e9fc7d3edaa398deaf
kevin <kevinculkin210@gmail.com>
2023-12-13 10:45:51 -0500

    commit 1000

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -10,6 +10,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.*;
 import java.security.cert.CertificateException;
+import java.sql.*;
+import java.util.Map;
 import java.util.Stack;
 
 import com.sun.net.httpserver.*;
@@ -20,21 +22,68 @@ import javax.net.ssl.SSLContext;
 public class Main {
 	public static final int portNumber = Integer.parseInt(System.getenv("PORT"));
 	public static void main(String[] args) throws IOException, InterruptedException, KeyStoreException, UnrecoverableKeyException, NoSuchAlgorithmException, KeyManagementException, CertificateException {
-        System.out.println("Started");
-        HttpServer server = HttpServer.create(new InetSocketAddress(portNumber), 0);
+		// JDBC URL, username, and password of the MySQL server
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			return;
+		}
+		HttpServer server = HttpServer.create(new InetSocketAddress(portNumber), 0);
         server.createContext("/home",(exchange->{
-            System.out.println("Received");
             Headers headers = exchange.getResponseHeaders();
             headers.set("Content-Type", "text/html");
         	exchange.sendResponseHeaders(200, 0);
         		try (OutputStream os = exchange.getResponseBody()) {
             os.write(Files.readAllBytes(new File("src/main/resources/static/home.html").toPath()));
-        } catch (Exception e) {e.printStackTrace();}
-            System.out.println("Sent");}));
+        } catch (Exception e) {e.printStackTrace();}}));
+
+        server.createContext("/project",(exchange->{
+            Headers headers = exchange.getResponseHeaders();
+            headers.set("Content-Type", "text/html");
+        	exchange.sendResponseHeaders(200, 0);
+        		try (OutputStream os = exchange.getResponseBody()) {
+            os.write(Files.readAllBytes(new File("src/main/resources/static/index.html").toPath()));
+        } catch (Exception e) {e.printStackTrace();}}));
+
+        server.createContext("/api",(exchange->{
+            exchange.getResponseHeaders().set("Content-Type", "text/plain");
+			exchange.getRequestHeaders();
+			Map<String, String> queryParams = getQueryParams(exchange.getRequestURI().getQuery());
+			String response = "";
+			for (Map.Entry<String, String> entry : queryParams.entrySet()){
+				try {
+					response += getSQL(entry.getValue());
+				} catch (SQLException e) {
+					throw new RuntimeException(e);
+				}
+			}
+        	exchange.sendResponseHeaders(200, 0);
+        		try (OutputStream os = exchange.getResponseBody()) {
+            		os.write(response.getBytes());
+        } catch (Exception e) {e.printStackTrace();}}));
         server.start();
-        System.out.println("Server Started");
         Thread.sleep(10000);
         System.out.println("Killed");
         System.exit(1337);
+	}
+	private static Map<String, String> getQueryParams(String query) {
+		return java.util.Arrays.stream(query.split("&"))
+				.map(s -> s.split("="))
+				.collect(java.util.stream.Collectors.toMap(s -> s[0], s -> s[1]));
+	}
+	private static String getSQL(String matcher) throws SQLException {
+		if (matcher.length()>10)
+			throw new RuntimeException();
+		try (Connection connection = DriverManager.getConnection("jdbc:postgresql://ec2-18-204-162-101.compute-1.amazonaws.com:5432/dda9u1msavb3nm",
+				"gemntditxmjbgp","8cbf73aebd2dcef3e7cb53ee76765f4dc018c2c9e971e314adeced39112ad296");
+			 Statement statement = connection.createStatement()) {
+			// Execute a simple query
+			String sqlQuery = "SELECT * FROM classes WHERE classname = '"+matcher+"'";
+			try (ResultSet resultSet = statement.executeQuery(sqlQuery)) {
+				System.out.println(resultSet);
+				return resultSet.toString();
+			}
+		} catch (SQLException e) {e.printStackTrace(); throw e;}
 	}
 }
